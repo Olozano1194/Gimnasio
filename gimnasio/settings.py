3a05d8c2cd802d5bb218b6f4581d768d5fec6689
Oscar Lozano <olozano1194@gmail.com>
2024-11-14 02:12:11 -0500

    Haciendo pruebas para poder ingresar a la aplicación

--- gimnasio/settings.py
+++ gimnasio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'django-insecure-&-0hm^g5cj5v*_dso6#iq%b+nc!qy9ptln16bn#jydu7a$^ge#'
-SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
+SECRET_KEY = os.environ.get('SECRET_KEY', default='yourSecretKey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
@@ -49,13 +49,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'gimnasio.urls'
@@ -94,7 +95,7 @@ DATABASES = {
     # Replace the SQLite DATABASES configuration with PostgreSQL:
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/gym_h1on',
+        default='postgresql://postgres:postgres@localhost:5432/gym',
         conn_max_age=600
     )
 }
